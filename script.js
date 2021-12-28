6fd3c26923863cdad8859aa360ca4184c7667cc0
udarange <udarange@gmail.com>
2021-12-28 19:34:26 +0530

    handle multiple env files with dotenv

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+  // require('dotenv').config({ path: `.env.staging` })
+  require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 }
 
 const Joi = require('Joi')
